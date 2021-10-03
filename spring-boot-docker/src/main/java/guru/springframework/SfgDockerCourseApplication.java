a79bad6a69449b8201f8426e5059fba84e2c3340
oscar.santamaria <osantamaria@gmail.com>
2021-10-03 11:18:13 -0600

    Design more robust application approach

--- spring-boot-docker/src/main/java/guru/springframework/SfgDockerCourseApplication.java
+++ spring-boot-docker/src/main/java/guru/springframework/SfgDockerCourseApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgDockerCourseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfgDockerCourseApplication.class, args);
 	}
