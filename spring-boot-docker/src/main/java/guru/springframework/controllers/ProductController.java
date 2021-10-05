4dc140091bcd304c626320d84015cc2d1db95027
oscar.santamaria <osantamaria@gmail.com>
2021-10-05 00:36:15 -0600

    Running Docker containers via Maven

--- spring-boot-docker/src/main/java/guru/springframework/controllers/ProductController.java
+++ spring-boot-docker/src/main/java/guru/springframework/controllers/ProductController.java
@@ -3,6 +3,8 @@ package guru.springframework.controllers;
 import guru.springframework.model.events.PageViewEvent;
 import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,8 +20,9 @@ import java.util.UUID;
 @Controller
 public class ProductController {
 
-    private ProductService productService;
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
+    private ProductService productService;
     private PageViewService pageViewService;
 
     @Autowired
@@ -30,12 +33,18 @@ public class ProductController {
 
     @RequestMapping("/product/{id}")
     public String getProductById(@PathVariable Integer id, Model model){
+
         model.addAttribute("product", productService.getProduct(id));
+
+        //Send Page view event
         PageViewEvent pageViewEvent = new PageViewEvent();
         pageViewEvent.setPageUrl("springframework.guru/product/" + id);
         pageViewEvent.setPageViewDate(new Date());
         pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        log.info("Sending Message to pagie view service");
         pageViewService.sendPageViewEvent(pageViewEvent);
+
         return "product";
     }
 }
